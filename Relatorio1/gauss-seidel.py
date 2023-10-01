cad245738e7b0e089fecebea7c8cc6c7a2430ea7
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-09-30 22:36:36 -0300

    add implementacao metodo de gauss-seidel

--- Relatorio1/gauss-seidel.py
+++ Relatorio1/gauss-seidel.py
@@ -1,44 +1,43 @@
 import sympy as sp
 import common
 
-def jacobi(arquivo_saida, matrizB, vetorG, precisao, vet_inical = None):
-    # verifica se a matriz B como é convergente 
-    if common.check_converge(matrizB):
-        n = sp.shape(matrizB)[0]
-        
-        # se vet_inical for None, vet_inical = vetor nulo
-        if vet_inical is None:
-            vet_inical = sp.zeros(n, 1)
+def gauss_seidel(arquivo_saida, matrizB, vetorG, precisao, vet_inical = None):
+    # verifica se a matriz B é convergente 
+    #if common.check_converge(matrizB):
+    n = sp.shape(matrizB)[0]
+    
+    # se vet_inical for None, vet_inical = vetor nulo
+    if vet_inical is None:
+        vet_inical = sp.zeros(n, 1)
 
-        k = 0 # contador de iterações
-        
-        common.escrever_arquivo(arquivo_saida, common.print_matriz(vet_inical, f"x{k}", 'n'))
-        common.escrever_arquivo(arquivo_saida, "\n")
-        # vet_0 recebe vet_inical
-        vet_0 = vet_inical.copy()
-        
-        # vet_1 vai receber o resultado da iteração
-        vet_1 = matrizB*vet_0 + vetorG
-        
-        # calcula erro absoluto e relativo
+    k = 0 # contador de iterações
+    
+    common.escrever_arquivo(arquivo_saida, common.print_matriz(vet_inical, f"x{k}", 'n'))
+    common.escrever_arquivo(arquivo_saida, "\n")
+    # vet_0 e vet_0 recebe vet_inical
+    vet_0 = vet_inical.copy()
+    vet_1 = vet_inical.copy()
+    
+    # calcula erro absoluto e relativo
+    
+    eabs = erel = precisao + 1 
+    # enquanto erro absoluto ou relativo for maior que precisao continua iteração
+    while(eabs > precisao or erel > precisao):
+        for i in range(n):
+            # calcula x(k+1) = Bx(k) + g
+            vet_1[i, 0] = (matrizB[i,:]*vet_1)[0,0] + vetorG[i, 0]
         eabs, erel = common.return_variacao(vet_1, vet_0)
-        
-        # enquanto erro absoluto ou relativo for maior que precisao continua iteração
-        while(eabs > precisao or erel > precisao):
-            vet_0 = vet_1.copy() # vet_0 recebe vet_1
-            k+=1
-            common.escrever_arquivo(arquivo_saida, common.print_matriz(vet_0, f"x{k}", 'n'))
-            common.escrever_arquivo(arquivo_saida, f"\t eabsoluto: {eabs:.5f} | erelativo: {erel:.5f}\n")
-            vet_1 = matrizB*vet_0 + vetorG # recalcula vet_1
-            eabs, erel = common.return_variacao(vet_1, vet_0) # recalcula erro absoluto e relativo
-        return vet_0
-    else:
-        return None
-
-
+        vet_0 = vet_1.copy() # vet_0 recebe vet_1
+        k+=1
+        common.escrever_arquivo(arquivo_saida, common.print_matriz(vet_0, f"x{k}", 'n'))
+        common.escrever_arquivo(arquivo_saida, f"\t eabsoluto: {eabs:.5f} | erelativo: {erel:.5f}\n")
+    return vet_0
+    #else:
+    #    return None
+    
 def main():
-    input = "G:\Meu Drive\\facul\\analise numerica\AnaliseNumerica\Relatorio1\input_jac.txt"
-    output = "G:\Meu Drive\\facul\\analise numerica\AnaliseNumerica\Relatorio1\output_jac.txt"
+    input = "G:\Meu Drive\\facul\\analise numerica\AnaliseNumerica\Relatorio1\inputs\gauss-seidel\input_gauss_seidel.txt"
+    output = "G:\Meu Drive\\facul\\analise numerica\AnaliseNumerica\Relatorio1\outputs\gauss-seidel\output_gauss-seidel.txt"
     
     
     entrada = common.abrir_entrada(input)
@@ -73,9 +72,8 @@ def main():
                 matrizA = matrizA.row_insert(i-4, sp.Matrix([entrada[i].split(' ')[0:]]))
             else:
                 matrizA = matrizA.row_insert(i-4, sp.Matrix([entrada[i-1].split(' ')[0:]]))
-        if x0_definido:
+        if not x0_definido:
             matrizA = matrizA.row_insert(n-1, sp.Matrix([entrada[len(entrada)-1].split(' ')[0:]]))
-        
     # verifica se o sistema tem solução
     if not common.check_sistema_solucao(matrizA, b, matrizX):
         return
@@ -101,11 +99,13 @@ def main():
         
         
         common.escrever_arquivo(arquivo_saida, "\n\nIteracoes:\n")
-        result = jacobi(arquivo_saida, matB, vetG, precisao, x0)
+        result = gauss_seidel(arquivo_saida, matB, vetG, precisao, x0)
         
-        common.escrever_arquivo(arquivo_saida, f"\nAproximacao com {precisao} de precisa: ")
-        common.escrever_arquivo(arquivo_saida, common.print_matriz(result, 'x', 'n'))
-        
+        if result is not None:
+            common.escrever_arquivo(arquivo_saida, f"\nAproximacao com {precisao} de precisa: ")
+            common.escrever_arquivo(arquivo_saida, common.print_matriz(result, 'x', 'n'))
+        else:
+            common.escrever_arquivo(arquivo_saida, "Matriz B nao eh convergente")
         
         
         arquivo_saida.close()
@@ -115,6 +115,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-    
-    
