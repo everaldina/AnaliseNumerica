74327f34bf88df966c728389940c7e4d34ab5e4c
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-10-24 20:27:32 -0300

    add aproximacao_polinomial_discreta

---
+++ src/Relatorio2/aprox_poli_discreta.py
@@ -0,0 +1,27 @@
+from .. import common
+import sympy as sp
+
+def aproximacao_polinomial(pontos):
+    n = len(pontos)
+    mat_UU = sp.zeros(n-1, n-1)
+    vet_yU = sp.zeros(n-1, 1)
+    vet_U = []
+    vet_a = sp.zeros(n-1, 1)
+    
+    # preenchendo vetores x e y
+    vet_y = sp.zeros(n, 1)
+    vet_x = sp.zeros(n, 1)
+    for i in range(n):
+        vet_y[i, 0] = pontos[i].y
+        vet_x[i, 0] = pontos[i].x
+        
+    # preenchendo matrizes U
+    vet_U.append(sp.ones(n, 1))
+    for i in range(1, n):
+        vet_U.append(vet_U[i-1].copy())
+        for j in range(n):
+            vet_U[i, j] *= vet_x[j, 0]
+    
+    
+    
+    
