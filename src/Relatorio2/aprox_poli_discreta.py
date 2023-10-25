0257eb78d4e0b94869dcf23ca50470457fbf5a53
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-10-25 11:02:17 -0300

    att aproximacao polinomial discreta

--- src/Relatorio2/aprox_poli_discreta.py
+++ src/Relatorio2/aprox_poli_discreta.py
@@ -20,7 +20,24 @@ def aproximacao_polinomial(pontos):
     for i in range(1, n):
         vet_U.append(vet_U[i-1].copy())
         for j in range(n):
-            vet_U[i, j] *= vet_x[j, 0]
+            vet_U[i][j, 0] *= vet_x[j, 0]
+    
+
+    for i in range(n-1):
+        # preenchendo matrizUU
+        for j in range(n-1):
+            if (i > j):
+                mat_UU[i, j] = mat_UU[j, i]
+            else:
+                mat_UU[i, j] = vet_U[i].dot(vet_U[j]) # calculando o produto interno
+        # vetor yU
+        vet_yU[i, 0] = vet_y.dot(vet_U[i])
+        
+    # resolvendo o sistema
+    common.result_sistema(mat_UU, vet_yU, vet_a)
+    
+    return vet_a    
+
     
     
     
