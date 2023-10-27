75177819b5f4ba0fd144a9ab1643c80add7f85ac
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-10-27 16:10:27 -0300

    implementação interpolacao de lagrange

--- src/Relatorio2/interpolacao_lagrange.py
+++ src/Relatorio2/interpolacao_lagrange.py
@@ -3,6 +3,13 @@ from .. import common
 
 x = sp.symbols('x')
 
+def interpolacao_lagrange(pontos):
+    n = len(pontos)
+    
+    # monta polinomio chamando lagrange para cada ponto e multiplicando pelo y do ponto
+    polinomio = 0
+    for i in range(n):
+        polinomio += pontos[i].y*lagrange(pontos, i)
 
 # retorna o polinomio de lagrange k, para n pontos, k = 0, 1, 2, ..., n-1
 def lagrange(pontos, k):
