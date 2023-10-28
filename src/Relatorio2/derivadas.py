220e1bb8b5c0f739f755ac3576cfa3ce59e627dd
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-10-27 21:19:11 -0300

    add derivada retardada

--- src/Relatorio2/derivadas.py
+++ src/Relatorio2/derivadas.py
@@ -10,4 +10,14 @@ def derivada_progressiva(expressao, ponto, h):
     
     # substitui os valores na formula da derivada
     return derivada.subs(f_x1, expressao.subs(x, ponto.x + h).evalf()).subs(f_x, expressao.subs(x, ponto.x).evalf()).subs(h, h).evalf()
+
+
+def derivada_retardada(expressao, ponto, h): 
+    f_x1, f_x, h = sp.symbols("f_x1 f_x h")
     
+    # formula da derivada retardada
+    derivada = sp.simplify("(f_x - f_x1)/h")
+    
+    # substitui os valores na formula da derivada
+    return derivada.subs(f_x1, expressao.subs(x, ponto.x - h).evalf()).subs(f_x, expressao.subs(x, ponto.x).evalf()).subs(h, h).evalf()
+
